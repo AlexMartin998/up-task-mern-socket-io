3f3e2cbb1dab87b875a33138be6cc7e5b15520a3
Adrian Alex <alextravc.342@gmail.com>
2022-07-13 15:00:41 -0500

    [FIX]: user routing corrected

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,3 +1,4 @@
 'use strict';
 
 export * from './auth.controller';
+export * from './user.controller';
---
+++ src/controllers/user.controller.js
@@ -0,0 +1,64 @@
+'use strict';
+
+import { User } from './../models';
+import { genId } from '../helpers';
+
+export const confirmUser = async (req, res) => {
+  try {
+    const { token } = req.params;
+    const unconfirmedUser = await User.findOne({ token });
+
+    unconfirmedUser.token = null;
+    unconfirmedUser.confirmed = true;
+    await unconfirmedUser.save();
+
+    res.status(200).json({
+      ok: true,
+      msg: 'Successful confirmation!',
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ ok: false, msg: 'Something went wrong!' });
+  }
+};
+
+export const generateRecoveryToken = async (req, res) => {
+  const { email } = req.body;
+  const user = await User.findOne({ email });
+  if (!user)
+    return res.status(401).json({ ok: false, msg: "User doesn't exist!" });
+
+  user.token = genId();
+  await user.save();
+
+  // Send email with token/instructions
+
+  res
+    .status(200)
+    .json({ ok: true, msg: 'An e-mail with instructions has been sent.' });
+};
+
+export const validateToken = async (_req, res) =>
+  res.status(200).json({ ok: true, msg: 'Successful validation!' });
+
+export const genNewPassword = async (req, res) => {
+  const { token } = req.params;
+  const { password } = req.body;
+
+  try {
+    const user = await User.findOne({ token });
+    user.token = null;
+    user.password = password;
+    await user.save();
+
+    res.status(201).json({ ok: true, msg: 'Password updated successfully!' });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ ok: false, msg: 'Something went wrong!' });
+  }
+};
+
+export const isAuthenticated = (req, res) => {
+  const { authenticatedUser } = req;
+  res.status(200).json({ ok: true, user: authenticatedUser });
+};
