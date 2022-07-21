a94714afea74edcb3625087b3418dfba9c79105a
Adrian Alex <alextravc.342@gmail.com>
2022-07-21 12:58:27 -0500

    [ADD]: socket.io added - add new task with sockets added

---
+++ src/sockets/sockets.js
@@ -0,0 +1,18 @@
+'use strict';
+
+export default io => {
+  io.on('connection', socket => {
+    console.log('New user connected!');
+
+    socket.on('client:openProject', projectId => {
+      socket.join(projectId); // 1 room x c/project
+    });
+
+    // Tasks
+    socket.on('client:createTask', newTask => {
+      const project = newTask.project;
+
+      socket.to(project).emit('server:addedTask', newTask); // Solo a los del mismo project/room
+    });
+  });
+};
