ddac8af8006ec624090314fa920ae2957d962670
Alex Martin <alextravc.342@gmail.com>
2022-09-30 20:53:10 -0500

    [ADD]: Dockerfile.dev added

--- src/helpers/genId.helper.js
+++ src/helpers/genId.helper.js
@@ -1,8 +1,7 @@
 'use strict';
 
 export const genId = () =>
-  (
-    performance.now().toString(32) +
-    Math.random().toString(32).substring(2) +
-    Date.now().toString(32)
-  ).replace('.', '');
+  (Math.random().toString(32).substring(2) + Date.now().toString(32)).replace(
+    '.',
+    ''
+  );
